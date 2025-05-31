e57cf6811d5b64f9869e53b123eecb42209a20e4
ebriancs <ebrian.cs@gmail.com>
2025-05-31 09:00:25 +0800

    updated

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -1,24 +1,18 @@
-import React, { useState, useEffect } from "react";
-import "./Personal.scss";
+import React, { useState, useEffect } from 'react';
+import './Personal.scss';
 
 const WEB3 = () => {
   return (
     <div className="web3">
       <p>
-        I'm a WEB3 enthusiast exploring the decentralized ecosystem. I trade
-        cryptocurrencies with modest capital to improve my analysis and
-        strategies. I also buy, sell, and play Axies, stake tokens and NFTs for
-        rewards, and join airdrops.
+        I'm a WEB3 enthusiast exploring the decentralized ecosystem. I trade cryptocurrencies with modest capital to improve my analysis and
+        strategies. I also buy, sell, and play Axies, stake tokens and NFTs for rewards, and join airdrops.
       </p>
       <p>
-        Check out my{" "}
-        <a
-          href="https://app.axieinfinity.com/profile/0x516a61a105694a184538e8f2ab7747def7b165a2/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        Check out my{' '}
+        <a href="https://app.axieinfinity.com/profile/0x516a61a105694a184538e8f2ab7747def7b165a2/" target="_blank" rel="noopener noreferrer">
           Inventory
-        </a>{" "}
+        </a>{' '}
         if you're interested in buying.
       </p>
     </div>
@@ -29,30 +23,42 @@ const Cybersec = () => {
   return (
     <div className="cyberSec">
       <p>
-        Cybersecurity is a passion of mine and a "shadow career." I've always
-        been intrigued by the art of hacking in all its forms. While my current
-        path as a developer offers broader opportunities, I still find the world
-        of cybersecurity fascinating. Although I can't commit myself fully to
-        hacking right now, I know that being a skilled programmer is crucial to
-        becoming a proficient hacker.
-        <br />
-        <br />
-        Who knows what the future holds? I'm curious to see where mine leads.
+        Cybersecurity is a passion of mine and a "shadow career." I have always been intrigued by the art of hacking in all its forms. While my
+        current path as a developer offers broader opportunities, I still find the world of cybersecurity fascinating. Although I cannot fully commit
+        to hacking right now, I know that being a skilled programmer is crucial to becoming a proficient hacker.
+        <p>Who knows what the future holds? I’m curious to see where mine will lead.</p>
       </p>
     </div>
   );
 };
 
-const Repair = () => {
+const Engineering = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const engineeringImagesContext = require.context('../assets/engineering', false, /\.(jpg|jpeg|png)$/);
+  const engineeringImages = engineeringImagesContext.keys().map((key) => engineeringImagesContext(key));
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * engineeringImages.length);
+      setCurrentIndex(randomIndex);
+    }, 2000);
+
+    return () => clearInterval(intervalId);
+  }, [engineeringImages.length]);
+
   return (
-    <div className="repair">
+    <div className="engineering">
       <p>
-        Repairing devices is something I want to explore as part of my interest
-        in technology. While it’s not a priority in my career, I enjoy learning
-        how things work and troubleshooting to fix them. Repair can be
-        challenging due to the cost of tools and parts, but the satisfaction
-        after a successful repair makes it all worthwhile.
+        Assembling and disassembling systems is something I am eager to explore further, as it lies at the core of engineering, which is understanding
+        how things work by taking them apart and putting them back together. While my focus has primarily been on software engineering, where I build
+        and break down complex systems digitally, I am also drawn to the physical side of engineering, whether it involves hardware, electronics, or
+        mechanical structures.
       </p>
+
+      <div className="images-container">
+        <img key={currentIndex} src={engineeringImages[currentIndex]} alt="" />
+      </div>
     </div>
   );
 };
@@ -61,12 +67,9 @@ const Music = () => {
   return (
     <div className="music">
       <p>
-        I find music theory fascinating. It’s amazing how each sound corresponds
-        to specific notes and how keys play a crucial role in playing
+        I find music theory fascinating. It’s amazing how each sound corresponds to particular notes and how keys play a crucial role in playing
         instruments and singing.
-        <br />
-        <br />I believe music has the power to heal, not just emotionally, but
-        even scientifically.
+        <p>I believe music has the power to heal, not only emotionally but also scientifically.</p>
       </p>
 
       <div className="video-container">
@@ -80,8 +83,7 @@ const Music = () => {
         ></iframe>
       </div>
       <p>
-        Music connects us to those universal principles, making it both an art
-        and a science.
+        Music connects us to those universal principles, making it both an art and a science.
         <br />
         <br />
         <div className="nikola-tesla-qoute">
@@ -95,11 +97,7 @@ const Music = () => {
 const Gallery = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const imagesContext = require.context(
-    "../assets/gallery",
-    false,
-    /\.(jpg|jpeg|png)$/
-  );
+  const imagesContext = require.context('../assets/gallery', false, /\.(jpg|jpeg|png)$/);
   const imageArray = imagesContext.keys().map((image) => imagesContext(image));
 
   useEffect(() => {
@@ -114,8 +112,7 @@ const Gallery = () => {
   return (
     <div className="gallery">
       <p>
-        This is a collection of me. While I’m usually hesitant to share photos
-        or content, but knowing that only those genuinely visit this site will
+        This is a collection of me. I’m usually hesitant to share photos or content, but knowing that only those who genuinely visit this site will
         see it gives me confidence.
       </p>
 
@@ -135,17 +132,17 @@ function Personal() {
 
   const contents = [
     {
-      title: "WEB3",
+      title: 'WEB3',
       data: <WEB3 />,
     },
-    { title: "Cybersec", data: <Cybersec /> },
-    { title: "Repair", data: <Repair /> },
+    { title: 'Cybersec', data: <Cybersec /> },
+    { title: 'Engineering', data: <Engineering /> },
     {
-      title: "Music",
+      title: 'Music',
       data: <Music />,
     },
     {
-      title: "Gallery",
+      title: 'Gallery',
       data: <Gallery />,
     },
   ];
@@ -164,14 +161,7 @@ function Personal() {
 
               {openContent === index && content.data}
 
-              <i
-                className={`${
-                  openContent === index
-                    ? "fas fa-chevron-up open"
-                    : "fas fa-chevron-down"
-                }`}
-                onClick={() => toggleContent(index)}
-              ></i>
+              <i className={`${openContent === index ? 'fas fa-chevron-up open' : 'fas fa-chevron-down'}`} onClick={() => toggleContent(index)}></i>
             </div>
           </div>
         ))}
